017e1376c96f0562133cc1290642fc4d48d7d208
Stanley Wu <skyeyester@gmail.com>
2014-06-14 17:56:11 +0800

    Initial commit

---
+++ test.rb
@@ -0,0 +1,23 @@
+# Examples of symbols
+:name 
+:a_symbol 
+:"surprisingly, this is also a symbol"
+
+#Basic Data Structures
+
+#Array
+A=[1, 2, 3, 4, 5]
+puts 'Array Example:'
+puts A[0]
+
+#Hashes
+#A hash, sometimes referred to as a dictionary, is a set of key-value pairs.
+B={:dog => 'barks', :cat => 'meows', :pig => 'oinks'}
+puts 'Hashes Example:'
+puts B[:pig]
+
+#Ruby expressions always return a value, even if that value is nil or an error
+a = puts "stuff" 
+puts 'Nil Example:'
+puts a
+
