fd1e060eea90048517d44a02882593dd3dfe173f
Stanley Wu <skyeyester@gmail.com>
2014-06-18 00:00:24 +0800

    This is the helloworld project

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+      get "welcome/say_hello" => "welcome#say" #map http://localhost:3000/welcome/say_hello to say Action in welcome Controller
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
